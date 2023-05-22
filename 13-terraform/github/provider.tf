403665b530982da9b1172a6d8d003d350f78bd40
Denis Zdor <Montade@mail.ru>
2023-05-22 20:34:50 +0300

    final test1

--- 13-terraform/github/provider.tf
+++ 13-terraform/github/provider.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   required_providers {
     github = {
@@ -10,17 +9,4 @@ terraform {
 
 provider "github" {
   token = var.GITHUB_token
-=======
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "5.25.1"
-    }
-  }
-}
-
-provider "github" {
-  token = var.GITHUB_token
->>>>>>> 45dadc30279fecedc8817a34f1230bca03f74ccf
 }
--- 13-terraform/github/repo.tf
+++ 13-terraform/github/repo.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 resource "github_repository_file" "file" {
 
   for_each            = fileset("${path.module}/../", "*.tf")
@@ -30,37 +29,4 @@ resource "github_repository_file" "git_file" {
   content             = file("${path.module}/../github/${each.value}")
   overwrite_on_create = true
   commit_message      = "by Denqe"
-=======
-resource "github_repository_file" "file" {
-
-  for_each            = fileset("${path.module}/../", "*.tf")
-  repository          = "devops-hometasks"
-  branch              = "master"
-  file                = "13-terraform/${each.value}"
-  content             = file("${path.module}/../${each.value}")
-  overwrite_on_create = true
-  commit_message      = "by Denqe"
-}
-
-resource "github_repository_file" "file1" {
-
-  for_each            = fileset("${path.module}/../", "*.tfvars")
-  repository          = "devops-hometasks"
-  branch              = "master"
-  file                = "13-terraform/${each.value}"
-  content             = file("${path.module}/../${each.value}")
-  overwrite_on_create = true
-  commit_message      = "by Denqe"
-}
-
-resource "github_repository_file" "git_file" {
-
-  for_each            = fileset("${path.module}/../github", "*.tf")
-  repository          = "devops-hometasks"
-  branch              = "master"
-  file                = "13-terraform/github/${each.value}"
-  content             = file("${path.module}/../github/${each.value}")
-  overwrite_on_create = true
-  commit_message      = "by Denqe"
->>>>>>> 45dadc30279fecedc8817a34f1230bca03f74ccf
 }
